b81df3b19f97d08e14ea23c56372a8e602eb58af
AlexiaMag1999 <ds19044@go.uop.gr>
2022-11-18 19:51:22 +0200

    Initial commit

---
+++ src/homeappliancesstore.java
@@ -0,0 +1,26 @@
+public class homeappliancesstore {
+
+
+    public static void main(String args[]){
+        String Eteria="";
+        String Dieythinsi="";
+        int Ypalili;
+
+        if(Eteria==null){}
+        if(Dieythinsi==null){}
+
+        if (args.length == 3)
+        {
+            System.out.println("To onoma eterias einai:");
+            System.out.println(args[0]);
+            System.out.println();
+
+            System.out.println("H dieythinsi tis eterias einai:");
+            System.out.println(args[1]);
+            System.out.println();
+
+            System.out.println("O arithmos twn ypalillvn einai:");
+            System.out.println(args[2]);
+        }
+    }
+}
